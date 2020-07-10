417ba49dd8d92f7b622398acff8d72a7a2cf2ae7
Clark Winkelmann <clark.winkelmann@gmail.com>
2020-07-11 01:03:24 +0200

    Use humanTime to format the accepted_at attribute. Fixes #22 Update javascript dependencies

--- js/src/forum/components/UserPoliciesStateModal.js
+++ js/src/forum/components/UserPoliciesStateModal.js
@@ -1,5 +1,5 @@
 import app from 'flarum/app';
-import extractText from 'flarum/utils/extractText';
+import humanTime from 'flarum/helpers/humanTime';
 import Modal from 'flarum/components/Modal';
 import sortByAttribute from '../../common/helpers/sortByAttribute';
 
@@ -20,9 +20,12 @@ export default class UserPoliciesStateModal extends Modal {
         return m('.Modal-body', m('ul', sortByAttribute(app.store.all('fof-terms-policies')).map(policy => {
             const state = this.props.user.fofTermsPoliciesState()[policy.id()];
 
-            return m('li', policy.name() + ': ' + (state && state.accepted_at ? extractText(app.translator.trans('fof-terms.forum.state-modal.accepted-at', {
-                date: moment(state.accepted_at).format(app.forum.attribute('fof-terms.date-format')) + ' (' + state.accepted_at + ')',
-            })) : app.translator.trans('fof-terms.forum.state-modal.not-accepted')));
+            return m('li', [
+                policy.name() + ': ',
+                state && state.accepted_at ? app.translator.trans('fof-terms.forum.state-modal.accepted-at', {
+                    date: humanTime(moment(state.accepted_at)),
+                }) : app.translator.trans('fof-terms.forum.state-modal.not-accepted'),
+            ]);
         })));
     }
 }
