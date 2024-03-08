27c4f08a0729b64fdd5385b78c29cbe67f87d19e
Sami Mazouz <sychocouldy@gmail.com>
2024-03-08 12:41:46 +0100

    perf: only load user state when showing full user data

--- src/Extenders/UserPoliciesRelationship.php
+++ src/Extenders/UserPoliciesRelationship.php
@@ -12,6 +12,7 @@
 namespace FoF\Terms\Extenders;
 
 use Flarum\Api\Serializer\BasicUserSerializer;
+use Flarum\Api\Serializer\CurrentUserSerializer;
 use Flarum\User\User;
 use FoF\Terms\Repositories\PolicyRepository;
 
@@ -33,6 +34,9 @@ class UserPoliciesRelationship
 
         if ($request->getAttribute('routeName') === 'users.show' && $serializer->getActor()->can('seeFoFTermsPoliciesState', $user)) {
             $attributes['fofTermsPoliciesState'] = $this->policies->state($user);
+        }
+
+        if ($serializer instanceof CurrentUserSerializer) {
             $attributes['fofTermsPoliciesHasUpdate'] = $this->policies->hasPoliciesUpdate($user);
             $attributes['fofTermsPoliciesMustAccept'] = $this->policies->mustAcceptNewPolicies($user);
         }
