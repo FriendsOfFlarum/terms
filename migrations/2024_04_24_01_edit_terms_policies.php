5634c9c9134cc9523d1a0a6e53ea9d7cbd269c81
Rafał Całka <calka.rafal@gmail.com>
2024-06-16 23:46:55 +0200

    fix: replace JSON column with longText for compatibility

--- migrations/2024_04_24_01_edit_terms_policies.php
+++ migrations/2024_04_24_01_edit_terms_policies.php
@@ -3,12 +3,12 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\Builder;
 
-// HINT: you might want to use a `Flarum\Database\Migration` helper method for simplicity!
-// See https://docs.flarum.org/extend/models.html#migrations to learn more about migrations.
 return [
     'up' => function (Builder $schema) {
         $schema->table('fof_terms_policies', function (Blueprint $table) {
-            $table->json('additional_info');
+            // This could be a JSON column type, but Flarum requires MySQL 5.6+ / MariaDB 10.0.5+.
+            // JSON columns are supported in MySQL 5.7+ / MariaDB 10.2+.
+            $table->longText('additional_info');
         });
     },
     'down' => function (Builder $schema) {
