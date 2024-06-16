02bf04be1caec52ebc026a7fce6d226344ab8fcc
rafaucau <rafaucau@users.noreply.github.com>
2024-06-16 22:11:20 +0000

    Apply fixes from StyleCI

--- migrations/2024_04_24_01_add_optional_policies_columns.php
+++ migrations/2024_04_24_01_add_optional_policies_columns.php
@@ -15,5 +15,5 @@ return Migration::addColumns('fof_terms_policies', [
     'optional' => ['boolean', 'default' => false],
     // This could be a JSON column type, but Flarum requires MySQL 5.6+ / MariaDB 10.0.5+.
     // JSON columns are supported in MySQL 5.7+ / MariaDB 10.2+.
-    'additional_info' => ['longText', 'nullable' => true]
+    'additional_info' => ['longText', 'nullable' => true],
 ]);
